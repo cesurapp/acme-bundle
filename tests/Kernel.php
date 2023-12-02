98b66fa7ec7bff4ba933fe6a4a60e4455d38ea54
Cesur APAYDIN <apaydin541@gmail.com>
2023-12-03 01:16:34 +0300

    Update Kernel.php

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -29,6 +29,21 @@ class Kernel extends BaseKernel
             'test' => true,
         ]);
 
+        // Doctrine Bundle Default Configuration
+        /*$container->extension('doctrine', [
+            'dbal' => [
+                'default_connection' => 'default',
+                'url' => 'sqlite:///%kernel.project_dir%/var/database.sqlite',
+            ],
+            'orm' => [
+                'auto_generate_proxy_classes' => false,
+                'enable_lazy_ghost_objects' => true,
+                'report_fields_where_declared' => true,
+                'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
+                'auto_mapping' => true,
+            ],
+        ]);*/
+        
         // Acme Bundle Default Configuration
         /* $container->extension('acme', []); */
     }
