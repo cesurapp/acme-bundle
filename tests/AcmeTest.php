b5ce5c0990c2dc667bdcad84e765162d2f5492f6
Cesur APAYDIN <apaydin541@gmail.com>
2025-07-04 00:00:07 +0700

    PHP84

--- tests/AcmeTest.php
+++ tests/AcmeTest.php
@@ -7,6 +7,13 @@ use Symfony\Component\HttpKernel\KernelInterface;
 
 class AcmeTest extends KernelTestCase
 {
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        restore_exception_handler();
+    }
+
     public function testAcme(): void
     {
         self::bootKernel();
